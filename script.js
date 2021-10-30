b154cf9592f6f4835c7dbbc820876b5d470a4d40
Corey <90798397+CoreyTattooArt@users.noreply.github.com>
2021-10-30 19:29:17 -0300

    Logica de comunicação com as divs foi alcançada
    função getText() está passando texto para dentro das divs

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-getText() {
-    const textbox = document.querySelector('.textbox')
-    const div = document.querySelector('.ex1')
-    const res1 = document.createElement('p')
-    const text =  textbox.value
-    console.log(text)
+function getText(){
+let inputValue = document.getElementById('send')
+let text = inputValue.value    
+let panel = document.querySelector('.panel')
+let boxes = document.getElementsByClassName("exemplos")
+console.log(boxes.length)
+for(let i = 0 ; i < boxes.length ; i++){
+    boxes[i].innerHTML = '';
+    boxes[i].innerHTML = `${text}`;
+}
+ 
+
+ 
+
+
+
+
+ 
 }
