092143be1972608378ae296d3fde6038943cfa8d
CoreyTattooArt <90798397+CoreyTattooArt@users.noreply.github.com>
2021-10-30 15:50:01 -0300

    codificaçao em andamento

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-
-
-skinLoad()
-
-getText()
-
+getText() {
+    const textbox = document.querySelector('.textbox')
+    const div = document.querySelector('.ex1')
+    const res1 = document.createElement('p')
+    const text =  textbox.value
+    console.log(text)
+}
