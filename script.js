ec5088ad4d3ff3f711fb25a88e8805788c657ffe
Corey <90798397+CoreyTattooArt@users.noreply.github.com>
2021-11-01 11:04:18 -0300

    alteraçoes de arquivos e nomes

--- script.js
+++ script.js
@@ -4,15 +4,22 @@ document.addEventListener("keypress", function(e){
     }
 })
 
+fetch("fontesGuerrilha.json")
+.then(res => res.json())
+.then(res => {
+   
+})  
 function getText(){
-let inputValue = document.getElementById('send')
-let text = inputValue.value    
-let panel = document.querySelector('.panel')
-let boxes = document.getElementsByClassName("exemplos")
-console.log(boxes.length)
-for(let i = 0 ; i < boxes.length ; i++){
-    boxes[i].innerHTML = '';
-    boxes[i].innerHTML = `${text}`;
-}
+ 
+    let inputValue = document.getElementById('send')
+    let text = inputValue.value    
+    let panel = document.querySelector('.panel')
+    let boxes = document.getElementsByClassName("exemplos")
+    
+  
+    for(let i = 0 ; i < boxes.length ; i++){
+        boxes[i].innerHTML = '';
+        boxes[i].innerHTML = `${text}`;
 
+    } 
 }
