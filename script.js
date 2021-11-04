4173f194860a62ffcc2d90895979dfb846de0dc8
Corey <90798397+CoreyTattooArt@users.noreply.github.com>
2021-11-03 21:12:28 -0300

    começando logica de filtragem

--- script.js
+++ script.js
@@ -5,18 +5,31 @@ document.addEventListener("keypress", function(e){
 })
 
 function getText(){
- 
+    let filterHand = document.getElementById('filterHand')
+    let filterTrad = document.getElementById('filterTrad')
+    let filterGoth = document.getElementById('filterGoth')
+    let filterFree = document.getElementById('filterFree')
+    let filterType = document.getElementById('filterType')
+    let filterCustom = document.getElementById('filterCustom')
+    let hand = document.getElementsByClassName('hand')
+    let trad = document.getElementsByClassName('trad')
+    let goth = document.getElementsByClassName('goth')
+    let free = document.getElementsByClassName('free')
+    let type = document.getElementsByClassName('type')
+ console.log(filterHand)
     let inputValue = document.getElementById('send')
     let text = inputValue.value    
     let panel = document.querySelector('.panel')
     let boxes = document.getElementsByClassName("exemplos")
     
+
   
     for(let i = 0 ; i < boxes.length ; i++){
         boxes[i].innerHTML = '';
         boxes[i].innerHTML = `${text}`;
-
-    } 
+    
+    }
+    filterHand.checked == true ? console.log('checked') : console.log('nao pegou') 
 }
 
 
